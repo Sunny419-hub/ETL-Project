cf6c4a32673b758dd6fcd650966914e52ea8401e
viviach <viviana.aragonch@gmail.com>
2021-06-10 21:19:13 -0500

    schema updated

--- schema.sql
+++ schema.sql
@@ -1,21 +1,34 @@
-﻿
+﻿DROP TABLE confirmed_cases; 
+DROP TABLE deaths;
+DROP TABLE vaccination;
+DROP TABLE tests;
+
+
+
 CREATE TABLE confirmed_cases (
     Country VARCHAR(30) NOT NULL PRIMARY KEY,
-    Total Confirmed Cases INT NOT NULL
+    Total_Confirmed INT NOT NULL,
+	Lat  double precision NOT NULL,
+	Long double precision NOT NULL
 );
 
-CREATE TABLE num_test (
+CREATE TABLE tests (
     Country VARCHAR(30) NOT NULL PRIMARY KEY,
-    Total Test INT NOT NULL
+    Total_Confirmed INT NOT NULL,
+	Lat  double precision NOT NULL,
+	Long double precision NOT NULL
 );
 
 CREATE TABLE deaths (
     Country VARCHAR(30) NOT NULL PRIMARY KEY,
-    Total Death Cases  INT  NOT NULL
+    Total_Deaths  INT  NOT NULL,
+	Lat  double precision NOT NULL,
+	Long double precision NOT NULL
 );
    
 CREATE TABLE vaccination (
     Country VARCHAR(30) NOT NULL PRIMARY KEY,
-    Total Vaccination INT  NOT NULL
+	Total_Vaccination double precision  NOT NULL,
+    People_fully_vaccinated double precision  NOT NULL
 );
    
