60857aabda7a6ee15da24bf1ef6c3ff09a82221e
viviach <viviana.aragonch@gmail.com>
2021-06-10 20:31:52 -0500

    sql schema to save data from dataframes

---
+++ schema.sql
@@ -0,0 +1,21 @@
+﻿
+CREATE TABLE confirmed_cases (
+    Country VARCHAR(30) NOT NULL PRIMARY KEY,
+    Total Confirmed Cases INT NOT NULL
+);
+
+CREATE TABLE num_test (
+    Country VARCHAR(30) NOT NULL PRIMARY KEY,
+    Total Test INT NOT NULL
+);
+
+CREATE TABLE deaths (
+    Country VARCHAR(30) NOT NULL PRIMARY KEY,
+    Total Death Cases  INT  NOT NULL
+);
+   
+CREATE TABLE vaccination (
+    Country VARCHAR(30) NOT NULL PRIMARY KEY,
+    Total Vaccination INT  NOT NULL
+);
+   
